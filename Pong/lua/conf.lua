3426c675886020b9f344c8c679645ca28c3f6dce
Luiz Fernando <68738153+Luiz-Wendland@users.noreply.github.com>
2022-04-03 03:31:46 -0300

    Add files via upload

---
+++ Pong/lua/conf.lua
@@ -0,0 +1,7 @@
+function love.conf(t)
+    t.title = "Pong"                --título do jogo (string)
+    t.version = "11.4"                  --versão de love que o jogo foi feito (string)
+    t.console = true                    --console do windows (boolean, windows apenas)
+    t.window.width = 1280               --largura da tela (number)
+    t.window.heigh = 720                --altura da tela (number)
+end
